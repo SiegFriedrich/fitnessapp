7963260cb26f4e4d2fb8c73842871c8d268980d8
Sieg Friedrich <siegfriedrich@outlook.com>
2024-12-08 19:16:35 +0900

    Recent Workout UI & HomeView Navigation

--- Fitnessapp/FitnessappApp.swift
+++ Fitnessapp/FitnessappApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct FitnessappApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            FitnessTabView()
         }
     }
 }
--- Fitnessapp/HomeView.swift
+++ Fitnessapp/HomeView.swift
@@ -18,6 +18,14 @@ struct HomeView: View {
     Activity(id:2, title:"Today steps", subtitle: "Goal 16,090", image:"figure.boxing", tintColor: .green, amount: "15,230"),
     Activity(id:3, title:"Today steps", subtitle: "Goal 14,010", image:"figure.cooldown", tintColor: .green, amount: "10,230")
     ]
+    var mockWorkoutList = [
+        Workout(id: 0, image: "figure.run", tintColor: .green, title: "Running", duration: "123 mins", date: "Aug 3", calories: "141 kcal"),
+        Workout(id: 1, image: "figure.boxing", tintColor: .blue, title: "Boxing", duration: "93 mins", date: "Oct 14", calories: "441 kcal"),
+        Workout(id: 2, image: "figure.cooldown", tintColor: .red, title: "Cooldown", duration: "70 mins", date: "Jul 3", calories: "3455 kcal"),
+        Workout(id: 3, image: "figure.play", tintColor: .brown, title: "Play", duration: "123 mins", date: "Mar 7", calories: "431 kcal"),
+        Workout(id: 4, image: "figure.basketball", tintColor: .orange, title: "Basketball", duration: "63 mins", date: "Feb 9", calories: "330 kcal")
+        
+    ]
     
     var body: some View {
         NavigationStack{
@@ -55,7 +63,7 @@ struct HomeView: View {
                                 ProgressCircleView(progress: $stand, goal: 6, color: .green)
                             }.padding(.all, 40)
                         }
-                    }.padding(.horizontal)
+                    }.padding(.horizontal).padding(.top)
                     
                     HStack{
                         Text("Fitness Activity").font(.title2)
@@ -81,7 +89,13 @@ struct HomeView: View {
                         NavigationLink{EmptyView()} label: {
                             Text("Show more").padding(.all, 10).foregroundStyle(.white).background(.blue).cornerRadius(20)
                         }
-                    }.padding(.horizontal)
+                    }.padding(.horizontal).padding(.top)
+                    
+                    LazyVStack{
+                        ForEach(mockWorkoutList, id: \.id){ workout in
+                            WorkoutCard(workout: workout)
+                        }
+                    }
                 }
             }
         }
