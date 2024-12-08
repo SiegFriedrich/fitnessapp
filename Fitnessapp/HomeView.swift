9a9d8f5a7ec8557a903059685533e322b4d06a88
Sieg Friedrich <siegfriedrich@outlook.com>
2024-12-08 12:17:44 +0900

    Home Title and Progress Circle

--- Fitnessapp/HomeView.swift
+++ Fitnessapp/HomeView.swift
@@ -8,6 +8,10 @@
 import SwiftUI
 
 struct HomeView: View {
+    @State var calories: Int = 300
+    @State var active: Int = 60
+    @State var stand: Int = 4
+    
     var body: some View {
         ScrollView(showsIndicators: false){
             VStack{
@@ -31,9 +35,19 @@ struct HomeView: View {
                             Text("8 hours").bold()
                         }
                     }
-                    
-                    
-                }
+                    Spacer()
+                    ZStack{
+                        VStack{
+                            ProgressCircleView(progress: $calories, goal: 600, color: .blue)
+                        }
+                        VStack{
+                            ProgressCircleView(progress: $active, goal: 360, color: .red)
+                        }.padding(.all, 20)
+                        VStack{
+                            ProgressCircleView(progress: $stand, goal: 6, color: .green)
+                        }.padding(.all, 40)
+                    }
+                }.padding()
                 
             }
         }
